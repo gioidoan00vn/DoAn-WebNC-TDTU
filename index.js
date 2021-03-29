7701a2ddc9aacf56bf49b43e0b3fda0c51faea94
trungtruc233 <65809378+trungtruc233@users.noreply.github.com>
2021-03-29 15:31:20 +0700

    3

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app= express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
-app.get('/', (req, res) => {
+app.get('/loginwithsv', (req, res) => {
     res.render('login_with_google')
 })
-
+app.get('/main', (req,res) => {
+    res.send('ok')
+})
 app.listen(3000, () => console.log('http://localhost:3000'))
