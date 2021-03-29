de59cf5ab5d2af42e0e65dd69e0279768ca5322b
trungtruc233 <65809378+trungtruc233@users.noreply.github.com>
2021-03-29 17:08:01 +0700

    aaaa

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+require('dotenv').config()
+
+
 const express= require('express');
 const app= express();
+
+require('./passport-setup')
+const passport = require('passport')
+
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
@@ -9,4 +16,6 @@ app.get('/loginwithsv', (req, res) => {
 app.get('/main', (req,res) => {
     res.send('ok')
 })
+app.use('/google',  passport.authenticate('google',{scope:['profile', 'email'] }))
+
 app.listen(3000, () => console.log('http://localhost:3000'))
