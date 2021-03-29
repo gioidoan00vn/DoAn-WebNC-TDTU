87b9ae2130a1f2187b793e3b76b0593c687fcbd5
trungtruc233 <65809378+trungtruc233@users.noreply.github.com>
2021-03-29 17:43:16 +0700

    gg

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ require('./passport-setup')
 const passport = require('passport')
 
 app.set('view engine', 'ejs');
+app.use(passport.initialize())
+
+app.use(passport.session())
 
 app.use(express.static('public'))
 app.get('/loginwithsv', (req, res) => {
@@ -19,6 +22,12 @@ app.get('/loginwithadmin',(req,res)=>{
 app.get('/main', (req,res) => {
     res.send('ok')
 })
+app.get('/success', (req, res) => {
+    res.render('profile')
+})
 app.use('/google',  passport.authenticate('google',{scope:['profile', 'email'] }))
-
+app.get('/google/callback', passport.authenticate('google',{failureRedirect:'/failed'}),
+(req, res) => {
+    res.redirect('/success')
+});
 app.listen(3000, () => console.log('http://localhost:3000'))
