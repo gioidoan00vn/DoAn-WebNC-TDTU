963653eb02d341a43dc014f2d799737cb1577117
trungtruc233 <65809378+trungtruc233@users.noreply.github.com>
2021-04-08 10:53:09 +0700

    d

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-
 require('dotenv').config()
-const express= require('express');
-const app= express()
-
+const express = require('express')
+const app = express()
 const passport = require('passport');
-
-const cookieSession = require('cookie-session');
-
+const cookieSession = require('cookie-session')
 require('./passport-setup');
 
+
+// For an actual app you should configure this with an experation time, better keys, proxy and secure
 app.use(cookieSession({
-    name:'tuto-session',
-    keys:['key1', 'key2']
-}))
+    name: 'tuto-session',
+    keys: ['key1', 'key2']
+  }))
 
+app.set('view engine','ejs')
 
-app.set('view engine', 'ejs');
-const isLoggedIn= (req,res, next) => {
-    if(req.user){
+// Auth middleware that checks if the user is logged in
+const isLoggedIn = (req, res, next) => {
+    if (req.user) {
         next();
-    }
-    else {
+    } else {
         res.sendStatus(401);
     }
 }
-app.get('/', (req,res) => {
-    res.render('login_with_google')
+
+// Initializes passport and passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Example protected and unprotected routes
+app.get('/', (req, res) => res.render('login_with_google'))
+app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+
+// In this route you can see that if the user is logged in u can acess his info in: req.user
+app.get('/good', isLoggedIn, (req, res) =>{
+    res.render("profile",{name:req.user.displayName,pic:req.user.photos[0].value,email:req.user.emails[0].value})
+    //res.send('ok')
 })
 
-app.get('/failed', (req, res) => {
-    res.send('You Failed to login')
-})
+// Auth Routes
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('/success', isLoggedIn, (req, res) => {
-    res.render('profile', {name:req.user.displayName, pic:req.user.photos[0].value, email: req.user.emails[0].value})
-})
-app.get('/google', passport.authenticate('google',{scope:['profile', 'email']}));
-
-app.get('/google/callback', passport.authenticate('google', {failureRedirect:'/failed'}),
-    function(req,res) {
-        res.redirect('/success');
-    }
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/good');
+  }
 );
 
 app.get('/logout', (req, res) => {
-    req.session= null;
+    req.session = null;
     req.logout();
     res.redirect('/');
 })
 
-app.listen(3000, () => console.log(`web app listening on port ${3000}`))
+app.listen(3000, () => console.log(`web app listening on port ${3000}!`))
